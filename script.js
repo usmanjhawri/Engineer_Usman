ae048d34b334c33f4e9d4a5018a90f9d7f79b62e
usmanjhawri <46955177+usmanjhawri@users.noreply.github.com>
2025-03-24 23:03:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-window.onload = function() {
-  alert("Welcome to my portfolio!");
-};
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
